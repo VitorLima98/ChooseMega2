0ff7ca8a365b953a6147a6252bd5bf069a1fba9c
VitorLima98 <vitor.lima.98@hotmail.com>
2023-06-28 08:54:19 -0300

    Update geral
    novo bg
    botao clickavvel, contador

---
+++ script.js
@@ -0,0 +1,16 @@
+var button = document.getElementById("clickme"),
+    count = 0, ready = true, pwr = 1;
+button.onclick = function () {
+    count += pwr;
+    button.innerHTML = "Cliques: " + count;
+    if (count % 5 === 0) ready = true;
+    if (count >= 500) alert(`Parabéns! ${count} pontos!`)
+};
+
+button.ondblclick = function () {
+    if (ready) {
+        pwr += 1;
+        button.innerHTML = "Power Up!";
+        ready = false;
+    }
+}
